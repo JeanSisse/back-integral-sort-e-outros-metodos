9f45d081a1bca8714684d22a565d8d5f032a62fb
JeanSisse <tavares1988@live.com>
2021-07-02 12:08:47 -0300

    Exercicio 'Filtrar maior string' resolvido

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,1 +1,4 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+const stringMaisLongo = cidades.reduce((acc, cidade) => acc.length > cidade.length ? acc : cidade);
+console.log(stringMaisLongo);

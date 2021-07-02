c5a8cc26cc25cdb360ae79552064e2c8609826dd
JeanSisse <tavares1988@live.com>
2021-07-02 12:37:21 -0300

    Exercicio 'Verificando limite de caracteres' resolvido

--- classe-04/index.js
+++ classe-04/index.js
@@ -1,1 +1,11 @@
 const palavras = ['livro', 'caneta', 'sol', 'carro', 'orelha'];
+
+
+function valida_array(elemento) {
+	
+	return elemento ? "existe palavra inválida" : "array válido";
+}
+
+const existe = palavras.some(x => x.length > 5);
+
+console.log(valida_array(existe));

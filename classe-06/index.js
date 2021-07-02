9f467785111e9541ffbf5a230571d3ef7e48025a
JeanSisse <tavares1988@live.com>
2021-07-02 12:52:58 -0300

    Exercicio 'Validação de números pares' resolvido

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,2 +1,9 @@
 const numeros = [0, 122, 4, 6, 7, 8, 44];
 
+function valida_array_dePares(ehValido) {
+	return ehValido ? "array válido" : "array inválido";
+}
+
+const ehValido = numeros.every(x => x % 2 === 0);
+
+console.log(valida_array_dePares(ehValido));

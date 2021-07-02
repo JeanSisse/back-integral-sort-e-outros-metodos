d1498b1cfeea63befee673b5863a7af99e43426d
JeanSisse <tavares1988@live.com>
2021-07-02 12:46:50 -0300

    Exercicio 'Validando lista de compras' resolvido

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,1 +1,14 @@
 const palavras = ["arroz", "feijão", "carne", "cerveja", "macarrão"];
+
+function valida_compras(argument) {
+	if (argument)
+		return "Revise sua lista, Joõa. Possui bebida com venda proibida!";
+	else
+		return "Tudo certo, vamos as compras!";
+}
+
+const validaListaDeCompra = palavras.some(a => 
+	(a === "cerveja" || a === "vodka"));
+
+
+console.log(valida_compras(validaListaDeCompra));
